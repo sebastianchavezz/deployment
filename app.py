e78fbd35e58c12531e0aafab393f5a941664b4e6
sebastianchavezz <sebastianchavez940@gmail.com>
2021-12-23 19:49:09 +0100

    better database

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-db_name = 'sqlite:///game.db'
+db_name = 'sqlite:///database2.db'
 
 
 
@@ -15,13 +15,38 @@ db = SQLAlchemy(app)
 
 class Game(db.Model):
 
-    __tablename__ = 'game'
+    __tablename__ = 'games'
 
-    type = db.Column(db.String)
+    index = db.Column(db.String)
     name = db.Column(db.String)
-    steam_appid = db.Column(db.Integer,primary_key=True)
+    steam_id = db.Column(db.Integer,primary_key=True)
+    developers = db.Column(db.String)
+    genre = db.Column(db.String)
+    reviews = db.Column(db.Integer)
+    positives = db.Column(db.Integer)
+    is_free = db.Column(db.Boolean)
+    prices_eu = db.Column(db.Float)
+    units_sold = db.Column(db.Integer)
+    final_revenue = db.Column(db.Integer)
     
-spel2 = Game.query.all()
+
+    def __init__(self,index,name,steam_appid,developers,genre,reviews,positives,is_free,price,units_sold,revenue,) -> None:
+        self.index= index
+        self.name=name
+        self.steam_id = steam_appid
+        self.developers = developers
+        self.genre = genre
+        self.reviews = reviews
+        self.positives = positives
+        self.is_free = is_free
+        self.prices_eu = price 
+        self.units_sold = units_sold
+        self.revenue = revenue
+        
+        
+
+
+
 
 
 @app.route('/')
